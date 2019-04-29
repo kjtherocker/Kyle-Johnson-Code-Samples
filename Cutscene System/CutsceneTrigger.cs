53701ba660db50d42600f167731c8811f16af8eb
KyleJohnson1 <kylejohnsonthegamedev@gmail.com>
2019-04-29 07:38:22 -0400

    Adding Cutscene Trigger

---
+++ Cutscene System/CutsceneTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneTrigger : MonoBehaviour {
+
+    GameManager m_GameManager;
+    bool m_HasEnteredTrigger;
+
+    public CutsceneController.Cutscenes m_Cutscenetoplay;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_GameManager = GameObject.FindObjectOfType<GameManager>();
+
+        m_HasEnteredTrigger = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (m_HasEnteredTrigger == false)
+        {
+            if (other.tag == "Player")
+            {
+                m_HasEnteredTrigger = true;
+                m_GameManager.cutsceneController.SetVideoClip(m_Cutscenetoplay);
+            }
+        }
+    }
+}
